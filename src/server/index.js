98d029e04e54e918cd53de94e8b203fcad7a9cf6
brian <briansegs@gmail.com>
2021-03-21 11:09:02 -0400

    feat: Add starter code

---
+++ src/server/index.js
@@ -0,0 +1,56 @@
+var path = require('path')
+const express = require('express')
+var cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+// Start up an instance of app
+const app = express();
+
+
+// to use json
+app.use(express.json())
+
+// to use url encoded values
+app.use(express.urlencoded({
+  extended: true
+}))
+
+// Cors for cross origin allowance
+app.use(cors())
+const { Router, response } = require('express');
+
+// Initialize the main project folder
+app.use(express.static('dist'))
+
+// Sets landing page
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+const port = 8081;
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
+})
+
+// Routes
+
+// GET All
+app.get('/all', (req, res) => {
+    res.send(projectData);
+})
+
+// POST Data
+app.post('/add', (req, res) => {
+    let newData = req.body;
+    let newEntry = {
+        temperature: newData.temperature,
+        date: newData.date,
+        userResponse: newData.feelings
+    };
+    projectData.push(newEntry);
+    console.log(projectData);
+})
