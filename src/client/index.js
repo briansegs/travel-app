290d523929713d43a49b99413b0f08b8eeb67a97
brian <briansegs@gmail.com>
2021-03-22 13:30:32 -0400

    refactor: Seperate nav styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import { action } from './js/app'
 
 import './styles/style.scss'
+import './styles/nav.scss'
 
 // EventListeners
 document.getElementById('generate').addEventListener('click', action);
