55d27c9fd3a43c11f594419fec593ebfa5bae16c
brian <briansegs@gmail.com>
2021-03-22 19:32:14 -0400

    feat: Add styles for mobile

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,8 @@ import { action } from './js/app'
 
 import './styles/style.scss'
 import './styles/nav.scss'
+import './styles/responsive.scss'
+import './styles/main.scss'
 
 // EventListeners
 document.getElementById('generate').addEventListener('click', action);
