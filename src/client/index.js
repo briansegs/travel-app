59e694f49cd70bb2eddf2ab46475f77a33de28b5
brian <briansegs@gmail.com>
2021-03-24 11:50:20 -0400

    feat: Add createOptions

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,12 @@
 import { action } from './js/app'
+import { creatOptions } from './js/optionsHandler'
 
 import './styles/style.scss'
 import './styles/nav.scss'
 import './styles/responsive.scss'
 import './styles/main.scss'
+
+creatOptions();
 
 // EventListeners
 document.getElementById('generate').addEventListener('click', action);
