0f1055169fae888abb8f7d3bbf5d72f4a581c4cc
brian <briansegs@gmail.com>
2021-03-25 14:43:00 -0400

    feat: Add app test

---
+++ __test__/app.test.js
@@ -0,0 +1,10 @@
+import { action } from '../src/client/js/app';
+
+
+describe('Test action', () => {
+
+    test('Check if action function is defined', () => {
+        expect(action).toBeDefined();
+    });
+
+})
