8475c60adf8610070ae7c6f4932393a0d5e7bd9e
brian <briansegs@gmail.com>
2021-03-25 20:27:58 -0400

    feat: handle errors

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -38,15 +38,29 @@ function action(e) {
     } else {
         getPixData(location)
             .then(function (pixData) {
-                addPixToDom(pixData);
+                if (pixData['hits'].length === 0) {
+                    getPixData('not found')
+                        .then (function (na) {
+                            addPixToDom(na);
+                        })
+                } else {
+                    addPixToDom(pixData);
+                }
             });
         getGeoData(e, location)
             .then(function (data) {
-                getwBitData(data)
-                    .then(function (data) {
-                        postData('/add', {data: data['data']});
-                        updateUI();
-                    });
+                let date = document.getElementById('date').value;
+                if (date === '') {
+                    alert('Date is missing.')
+                } else {
+                    getwBitData(data)
+                        .then(function (data) {
+                            postData('/add', {data: data['data']});
+                            updateUI(date);
+                        });
+                }
+
+
             });
     }
 }
@@ -90,11 +104,10 @@ const postData = async (url = '', data = {}) => {
  * Either posts a group of elements to the section or a single one
  * Sends an error if anything fails
  */
-const updateUI = async () => {
+const updateUI = async (date) => {
     const request = await fetch('/all');
     try {
         let weatherSec = document.querySelector('.col-full');
-        let date = document.getElementById('date').value;
         const allData = await request.json();
         const latest = allData[allData.length - 1];
         let wBitData = latest['data'];
