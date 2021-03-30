f07938a909fe612fa3bfae1e732e75d55c1fa645
brian <briansegs@gmail.com>
2021-03-29 22:10:53 -0400

    fix: File path

--- src/server/index.js
+++ src/server/index.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + '/dist'))
 
 // Sets landing page
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join('/app' + '/dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
